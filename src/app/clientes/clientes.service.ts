e7fbff5e48183236b00b0938e0577006ab0cec65
Miguel <miguelgomezaguirre@hotmail.com>
2020-01-04 18:44:40 -0300

    coneccion con el backend

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -2,16 +2,20 @@ import { CLIENTES } from './clientes.json';
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  private urlEndpoint = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndpoint);
   }
 
 }
